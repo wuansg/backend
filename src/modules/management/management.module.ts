26b30eb6c9a31e16b943e48c4073e0c267c12b59
wuansg <wuansg@users.noreply.github.com>
2026-09-13 05:00:15 +0000

    fix: provide cqrs to management guards

--- src/modules/management/management.module.ts
+++ src/modules/management/management.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { CqrsModule } from '@nestjs/cqrs';
 
 import { ManagementController } from './management.controller';
 import { ManagementService } from './management.service';
 
 @Module({
+    imports: [CqrsModule],
     controllers: [ManagementController],
     providers: [ManagementService],
 })
